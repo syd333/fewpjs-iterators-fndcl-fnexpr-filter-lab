2eda0d6c6ec4f1b2e9b94029d3e6f33f80afaeae
syd333 <sydmiranda@icloud.com>
2021-03-05 10:33:48 -0800

    commit tree 2b527800d701023e081877456149491db54f7698

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, name) {   
+   let result = array.filter(arr => arr.toLowerCase() === name.toLowerCase())
+   return result
+}
+
+function fuzzyMatch(array, string){
+    let result = array.filter(arr  => arr[0] === string[0])
+    return result
+}
+
+function matchName(array, string){
+    let result = array.filter(arr => arr.name === string)
+    return result
+}
